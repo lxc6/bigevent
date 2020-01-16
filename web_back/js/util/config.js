4fe39dd8982f0d05f828becde1af8070da0dceaf
天明 <1154706249@qq.com>
2020-01-16 17:57:29 +0800

    退出登录+获取管理员信息

---
+++ web_back/js/util/config.js
@@ -0,0 +1,12 @@
+// 配置文件
+//把项目的所有接口都放到此处
+
+// 所有接口基地址
+var BASEURL = 'http://192.168.172.82:8000';
+// 接口信息
+var APILIST = {
+    user_login: BASEURL + '/admin/login',
+    user_logout: BASEURL + '/admin/logout',
+    user_getuser: BASEURL + '/admin/getuser',
+
+}
