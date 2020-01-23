043838ddc8ed4794a4c18d1e49f08ab8d5d81c05
天明 <1154706249@qq.com>
2020-01-24 00:03:23 +0800

    类别管理的查看，增加，删除

--- web_back/js/util/config.js
+++ web_back/js/util/config.js
@@ -2,11 +2,19 @@
 //把项目的所有接口都放到此处
 
 // 所有接口基地址
-var BASEURL = 'http://192.168.172.82:8000';
+var BASEURL = 'http://192.168.0.102:8000';
 // 接口信息
 var APILIST = {
+    // 登录界面
     user_login: BASEURL + '/admin/login',
     user_logout: BASEURL + '/admin/logout',
     user_getuser: BASEURL + '/admin/getuser',
 
+    // 类别管理
+    // 获取
+    category_get: BASEURL + '/admin/category_search', //获取分类
+    // 添加
+    category_add: BASEURL + '/admin/category_add',
+    // 删除
+    category_del: BASEURL + '/admin/category_delete',
 }
